6ace7bcd5506cf122ef9a00fa2e32ea9f087aeba
6210110081 <6210110081@psu.ac.th>
2021-08-13 14:30:33 +0700

    Genarate Model

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:midterm/model/transaction.dart';
 import 'package:midterm/screen/home.dart';
 import 'package:midterm/screen/list.dart';
 import 'package:midterm/screen/profile.dart';
 import 'package:salomon_bottom_bar/salomon_bottom_bar.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Hive.initFlutter();
+
+  Hive.registerAdapter(TransactionAdapter());
+
   runApp(MyApp());
 }
 
